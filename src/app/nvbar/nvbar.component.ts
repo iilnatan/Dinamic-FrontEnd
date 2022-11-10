fb603400edc3505391261bd7b960b11e7f9e20fa
iilnantan <marcoxbuffax@hotmail.com>
2022-11-10 15:31:02 -0300

    adding Navbar and medias

---
+++ src/app/nvbar/nvbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nvbar',
+  templateUrl: './nvbar.component.html',
+  styleUrls: ['./nvbar.component.css']
+})
+export class NvbarComponent implements OnInit {
+
+  constructor() { }
+    //navbar
+    ArgProg:String= "../img/navbar/ArgProgIcon.png"
+    Home:String= "../img/navbar/Home.png"
+    About:String= "../img/navbar/About.png"
+    Contact:String= "../img/navbar/Contact.png"
+    //medias
+    GitHub:String="../img/medias/github.png"
+    Linkedin:String="../img/medias/linkedin.png"
+    Instagram:String="../img/medias/instagram.png"
+    Twitter:String="../img/medias/twitter.png"
+  
+
+  ngOnInit(): void {
+  }
+
+}
